eb312ca7fb48537267904fa20bc4af9f75ca2626
hsyed01 <me@hsyed.dev>
2021-10-28 15:38:58 -0400

    adding restfull api to get PR info and commits for every open PR

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,70 @@
-console.log('Hello world!');
+import { createServer, IncomingMessage, ServerResponse } from 'http';
+import axios from 'axios';
+
+const port = 5000;
+
+const axiosServices = axios.create({
+  headers: {
+    'Accept': 'application/vnd.github.v3+json',
+    'Access-Control-Allow-Origin': '*'
+  }
+});
+
+// response
+axiosServices.interceptors.response.use(
+  (response) => {
+    const res = response.data;
+    return Promise.resolve(res);
+  },
+  (error) => Promise.reject(error)
+);
+
+const server = createServer((req: IncomingMessage, res: ServerResponse) => {
+  switch (req.url) {
+    case '/': {
+      if (req.method === 'GET') {
+        axiosServices.get('https://api.github.com/repos/colinhacks/zod/pulls')
+        .then(async (prResInfo: any) => {
+          let prCommitsData: any[] = await Promise.all(
+            prResInfo.map((pr: any, index: number) => {
+              return new Promise((resolve, reject) => {
+                axiosServices.get(pr.commits_url)
+                .then((prCommitsResInfo: any) => {
+                  resolve({
+                    type: 'success',
+                    value: { 
+                      commitsUrl: pr.commits_url,
+                      commitsCount: prCommitsResInfo.length
+                    }
+                  });
+                }).catch((error: any) => {
+                  reject({
+                    type: 'failure',
+                    error
+                  });
+                });
+              });
+            })
+          );
+          console.log('<-------Pull Request Info---->', prResInfo);
+          console.log('<-------Commits Info for every Pull Request---->', prCommitsData);
+          res.writeHead(200, { 'Content-Type': 'application/json' });
+          res.write(JSON.stringify({prResInfo, prCommitsData}));
+          res.end();
+        })
+        .catch(error => {
+          console.log(error);
+        });
+      }
+      break;
+    }
+    default: {
+      res.statusCode = 404;
+      res.end();
+    }
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
