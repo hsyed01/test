ef50ffe79bb5f939e277ee9b679b026b10429eac
hsyed01 <me@hsyed.dev>
2021-10-28 22:25:07 -0400

    making an interface that accepts a repository URL

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import { createServer, IncomingMessage, ServerResponse } from 'http';
 import axios from 'axios';
 
 const port = 5000;
-
+const BASE_URL = 'https://api.github.com/repos/';
 const axiosServices = axios.create({
+  baseURL: BASE_URL,
   headers: {
     'Accept': 'application/vnd.github.v3+json',
     'Access-Control-Allow-Origin': '*'
@@ -22,45 +23,49 @@ axiosServices.interceptors.response.use(
 export class Server {
   public startServer() {
     createServer((req: IncomingMessage, res: ServerResponse) => {
-      switch (req.url) {
-        case '/': {
-          if (req.method === 'GET') {
-            axiosServices.get('https://api.github.com/repos/colinhacks/zod/pulls')
-            .then(async (prResInfo: any) => {
-              let prCommitsData: any[] = await Promise.all(
-                prResInfo.map((pr: any, index: number) => {
-                  return new Promise((resolve, reject) => {
-                    axiosServices.get(pr.commits_url)
-                    .then((prCommitsResInfo: any) => {
-                      resolve({
-                        type: 'success',
-                        value: { 
-                          commitsUrl: pr.commits_url,
-                          commitsCount: prCommitsResInfo.length
-                        }
-                      });
-                    }).catch((error: any) => {
-                      reject({
-                        type: 'failure',
-                        error
-                      });
+      const gitURL = req.url?.replace (/^[a-z]{4}\:\/{2}[a-z]{1,}\:[0-9]{1,4}.(.*)/, '$1');
+      // Create a regex to match protocol, domain, and host
+      const matchProtocolDomainHost = /^.*\/\/[^\/]+:?[0-9]?\//i;
+      // Replace protocol, domain and host from url, assign to `myNewUrl`
+      const userRepoUrl = gitURL?.replace(matchProtocolDomainHost, '');
+      console.log('userRepoUrl', userRepoUrl)
+      if (userRepoUrl !== '/') {
+        if (req.method === 'GET') {
+          axiosServices.get(userRepoUrl + '/pulls')
+          .then(async (prResInfo: any) => {
+            let prCommitsData: any[] = await Promise.all(
+              prResInfo.map((pr: any, index: number) => {
+                return new Promise((resolve, reject) => {
+                  const commitsUrl = pr.commits_url.replace(BASE_URL, '');
+                  axiosServices.get(commitsUrl)
+                  .then((prCommitsResInfo: any) => {
+                    resolve({
+                      type: 'success',
+                      value: { 
+                        commitsUrl: pr.commits_url,
+                        commitsCount: prCommitsResInfo.length
+                      }
+                    });
+                  }).catch((error: any) => {
+                    reject({
+                      type: 'failure',
+                      error
                     });
                   });
-                })
-              );
-              console.log('<-------Pull Request Info---->', prResInfo);
-              console.log('<-------Commits Info for every Pull Request---->', prCommitsData);
-              res.writeHead(200, { 'Content-Type': 'application/json' });
-              res.write(JSON.stringify({prResInfo, prCommitsData}));
-              res.end();
-            })
-            .catch(error => {
-              console.log(error);
-            });
-          }
-          break;
+                });
+              })
+            );
+            console.log('<-------Pull Request Info---->', prResInfo);
+            console.log('<-------Commits Info for every Pull Request---->', prCommitsData);
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.write(JSON.stringify({prResInfo, prCommitsData}));
+            res.end();
+          })
+          .catch(error => {
+            console.log(error);
+          });
         }
-        default: {
+        else {
           res.statusCode = 404;
           res.end();
         }
